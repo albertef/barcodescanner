a489269036887d249f168a5184c2ad2920c818d7
Albert Francis <albertef@gmail.com>
2017-03-09 14:57:17 +0530

    Cordova project started for barcode scanner

--- barcodescanner/platforms/android/assets/www/js/index.js
+++ barcodescanner/platforms/android/assets/www/js/index.js
@@ -1,46 +1,45 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+window.onload = function() {
+    document.addEventListener('deviceready', onDeviceReady, false);
+	barcodescan();
+}
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+function onDeviceReady() {
+    this.receivedEvent('deviceready');
+	this.barcodescan();
+}
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+function receivedEvent(id) {
+    var parentElement = document.getElementById(id);
+    var listeningElement = parentElement.querySelector('.listening');
+    var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
-    }
-};
-
-app.initialize();
+    listeningElement.setAttribute('style', 'display:none;');
+    receivedElement.setAttribute('style', 'display:block;');
+}
+	
+function barcodescan() {
+	alert(8);
+	cordova.plugins.barcodeScanner.scan(
+		alert(9);
+		function (result) {
+			alert("We got a barcode\n" +
+			"Result: " + result.text + "\n" +
+			"Format: " + result.format + "\n" +
+			"Cancelled: " + result.cancelled);
+		},
+		function (error) {
+			alert("Scanning failed: " + error);
+		},
+		{
+			preferFrontCamera : true,
+			showFlipCameraButton : true,
+			showTorchButton : true,
+			torchOn: true,
+			prompt : "Place a barcode inside the scan area",
+			resultDisplayDuration: 500,
+			formats : "QR_CODE,PDF_417",
+			orientation : "landscape"
+		}
+	);
+}
+	
