71d04eab8f3612d84b08cb0d13926f3d10c5b01b
Albert Francis <albertef@gmail.com>
2017-03-12 02:53:34 +0530

    Barcode scanning done successfully

--- barcodescanner/platforms/android/assets/www/js/index.js
+++ barcodescanner/platforms/android/assets/www/js/index.js
@@ -1,5 +1,6 @@
 window.onload = function() {
     document.addEventListener('deviceready', onDeviceReady, false);
+	document.getElementById('encoderesult').innerHTML="<img src='img/squares.gif' class='loadingimage'></p>"
 }
 
 function onDeviceReady() {
@@ -10,20 +11,17 @@ function barcodescan() {
 	cordova.plugins.barcodeScanner.scan(
 		
 		function (result) {
-			console.log("test");
-			var out = "We got a barcode\n" +
-			"Result: " + result.text + "\n" +
-			"Format: " + result.format + "\n" +
-			"Cancelled: " + result.cancelled;
+			document.getElementById("outputcard").className += " show";
+			document.getElementById("homepageicons").className += " active";
+			document.getElementById("barcodevalue").innerHTML = result.text;
+			document.getElementById("barcodeformat").innerHTML = result.format;
 			
-			document.getElementById("barcoderesult").innerHTML = out;
-			
-			cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, result.text, function(success) {
+			/*cordova.plugins.barcodeScanner.encode(cordova.plugins.barcodeScanner.Encode.TEXT_TYPE, result.text, function(success) {
 				alert("encode success: " + success);
 			  }, function(fail) {
 				alert("encoding failed: " + fail);
 			  }
-			);
+			);*/
 		},
 		function (error) {
 			alert("Scanning failed: " + error);
@@ -40,4 +38,51 @@ function barcodescan() {
 		}
 	);
 }
+
+function cardclose() {
+	document.getElementById("outputcard").className = "row outputcard";
+	document.getElementById("homepageicons").className = "homepageicons";
+	document.getElementById("encoderesult").innerHTML = "";
+}
+
+function encodebarcode() {
+	var result = document.getElementById("barcodevalue").innerHTML;
+	document.getElementById('encoderesult').innerHTML="<img src='img/squares.gif' class='loadingimage'></p>"
+	document.getElementById('encoderesult').style.opacity = 1;
+	
+	var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+            if (xmlhttp.status == 200) {
+			    var jsonout = JSON.parse(xmlhttp.responseText);
+				if(jsonout.status.message != "Product not found") {
+					jsonoutput = '<p><h5>' + jsonout.product.attributes.product + '</h5></p>' +
+							  '<p>' + jsonout.product.attributes.category_text + '</p>' +
+							  '<p>' + jsonout.product.attributes.long_desc + '</p>' +
+							  '<p>' + jsonout.product.attributes.features + '</p>' +
+							  '<p>' + jsonout.product.attributes.binding + '</p>';
+							  
+					document.getElementById('encoderesult').innerHTML = "<p><img src=" + jsonout.product.image +"></p>";
+					document.getElementById('encoderesult').innerHTML += jsonoutput;
+				}
+				else {
+					document.getElementById('encoderesult').innerHTML = '<p><h5>' + jsonout.status.message + '!</h5></p>';
+				}
+           }
+           else if (xmlhttp.status == 400) {
+              alert('There was an error 400');
+           }
+           else {
+               alert('Please check your internet connectivity');
+           }
+        }
+    };
+
+    xmlhttp.open("GET", "http://eandata.com/feed/?v=3&keycode=7F35D39458C459F1&mode=json&find=" + result, true);
+    xmlhttp.send();
+}
+
+//7F35D39458C459F1
+//
 	
